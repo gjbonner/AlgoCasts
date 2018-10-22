cf28ad721eee13958a6594ef78d5aa9dcd99496e
unknown <gjbonner@gmail.com>
2018-10-22 10:27:28 -0400

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
-
+function capitalize(str){
+ let cap = []
+  for(word of str.split(' ')){
+    cap.push(word.slice(0,1).toUpperCase() + word.slice(1, word.length))
+  }
+  return cap.join(' ')
+}
 module.exports = capitalize;
