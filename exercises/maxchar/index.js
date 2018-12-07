6488f940f2e510a67dd4952a2897d853457e7eed
unknown <gjbonner@gmail.com>
2018-12-07 12:14:59 -0500

    chunked

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let chars = {}
+  let maxChar = ''
+  let max = 0
+  for(c of str.split('')){
+    if(!chars[c]){
+      chars[c] = 1
+    } else {
+      chars[c]++
+      maxChar = chars[c]
+    }
+  }
+  for(char in chars){
+    if(chars[char] > max){
+      max = chars[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
