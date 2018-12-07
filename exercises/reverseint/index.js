08a22b6ae4402528160f81bad7841051bf4b40b0
unknown <gjbonner@gmail.com>
2018-12-07 11:57:42 -0500

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,7 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  
+  const reversed = n.toString().split('').reverse().join('')
+  return n < 0 ? parseInt(reversed) * -1 : parseInt(reversed)
 }
 
 module.exports = reverseInt;
